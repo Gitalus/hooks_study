210639c31e0aab7eea351f858214b7616ce3126a
Italo Guajardo <italog.penaloza@gmail.com>
2021-08-30 16:28:13 -0400

    change name in custom hook from state to counter

--- src/components/01-useState/CounterWithCustomHook.js
+++ src/components/01-useState/CounterWithCustomHook.js
@@ -4,15 +4,15 @@ import './counter.css';
 
 export const CounterWithCustomHook = () => {
 
-    const { state, increment, decrement } = useCounter();
+    const { state: counter, increment, decrement } = useCounter();
 
     return (
         <>
-            <h1>Counter with Hook: { state }</h1>
+            <h1>Counter with Hook: { counter }</h1>
             <hr />
 
-            <button className="btn" onClick={ () => { increment(state + 1) } }> + 1 </button>
-            <button className="btn" onClick={ () => { decrement(state - 1) } }> - 1 </button>
+            <button className="btn" onClick={ () => { increment(counter + 1) } }> + 1 </button>
+            <button className="btn" onClick={ () => { decrement(counter - 1) } }> - 1 </button>
         </>
     )
 }
