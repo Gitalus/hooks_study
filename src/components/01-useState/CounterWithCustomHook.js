0fbd6722a779d4d49926180bbd2ce7f5e79481d5
Italo Guajardo <italog.penaloza@gmail.com>
2021-08-30 17:09:43 -0400

    add reset button with functionality in custom hook

--- src/components/01-useState/CounterWithCustomHook.js
+++ src/components/01-useState/CounterWithCustomHook.js
@@ -4,15 +4,16 @@ import './counter.css';
 
 export const CounterWithCustomHook = () => {
 
-    const { state: counter, increment, decrement } = useCounter();
+    const { state, increment, decrement, reset } = useCounter();
 
     return (
         <>
-            <h1>Counter with Hook: { counter }</h1>
+            <h1>Counter with Hook: { state }</h1>
             <hr />
 
-            <button className="btn" onClick={ () => { increment() } }> + 1 </button>
-            <button className="btn" onClick={ () => { decrement() } }> - 1 </button>
+            <button className="btn" onClick={ () => increment(2) }> + 1 </button>
+            <button className="btn" onClick={ reset }> Reset </button>
+            <button className="btn" onClick={ () => decrement(2) }> - 1 </button>
         </>
     )
 }
