b4c799164d6e0e524098a093631edf63d1decb48
Italo Guajardo <italog.penaloza@gmail.com>
2021-08-30 15:54:57 -0400

    change button to only affect one propertie of the state

--- src/components/01-useState/CounterApp.js
+++ src/components/01-useState/CounterApp.js
@@ -3,16 +3,24 @@ import './counter.css'
 
 export const CounterApp = () => {
 
-    const [counter, setCounter] = useState(10)
-    
+    const [ { counter1, counter2 }, setCounter ] = useState({ 
+        counter1: 10,
+        counter2: 20
+    });
+
     return (
         <div>
-            <h1>Counter { counter }</h1>
+            <h1>Counter { counter1 }</h1>
+            <h1>Counter { counter2 }</h1>
             <hr />
 
             <button 
                 className="btn btn-primary"
-                onClick={ () => { setCounter(counter + 1) }}>
+                onClick={ () => { setCounter( c => ({
+                    ...c,
+                    counter1: counter1 + 1 
+                }))}}
+                >
                 +1
             </button>
         </div>
