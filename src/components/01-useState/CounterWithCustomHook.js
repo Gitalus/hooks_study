ded42ab461bf72aaec3b6f295fabc97a69a0bf1e
Italo Guajardo <italog.penaloza@gmail.com>
2021-08-30 16:26:27 -0400

    create full functionality with custom hook

--- src/components/01-useState/CounterWithCustomHook.js
+++ src/components/01-useState/CounterWithCustomHook.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import { useCounter } from '../../hooks/useCounter';
 import './counter.css';
 
 export const CounterWithCustomHook = () => {
 
-    
+    const { state, increment, decrement } = useCounter();
 
     return (
         <>
-            <h1>Counter with Hook: { 0 }</h1>
+            <h1>Counter with Hook: { state }</h1>
             <hr />
 
-            <button className="btn"> + 1 </button>
-            <button className="btn"> - 1 </button>
+            <button className="btn" onClick={ () => { increment(state + 1) } }> + 1 </button>
+            <button className="btn" onClick={ () => { decrement(state - 1) } }> - 1 </button>
         </>
     )
 }
