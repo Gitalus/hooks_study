38cc6e7bac30ac12d8fa80139885b07dc660534f
Italo Guajardo <italog.penaloza@gmail.com>
2021-09-01 17:36:22 -0400

    make a conditional render for loading

--- src/components/03-examples/MultipleCustomHooks.js
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -4,11 +4,28 @@ import '../02-useEffect/effects.css';
 
 export const MultipleCustomHooks = () => {
 
-    const state = useFetch( `https://www.breakingbadapi.com/api/quotes/1` );
-    console.log(state);
+    const { loading, data } = useFetch( `https://www.breakingbadapi.com/api/quotes/1` );
+
+    const { author, quote } = !!data && data[0]; // Doble negación para asignar undefined y no null
+
+    console.log( quote, author );
+
     return (
         <div>
-            <h1>Custom Hooks!!</h1>
+            <h1>BreakingBad Quotes</h1>
+            <hr />
+            {
+                loading ? (
+                    <div className="alert alert-info text-center">
+                        Loading...
+                    </div>
+                ) : (
+                    <blockquote className="blockquote text-end">
+                        <p className="">Hola Mundo</p>
+                        <footer className="blockquote-footer">Italo</footer>
+                    </blockquote>
+                )
+            }
         </div>
     )
 }
