65ecc14a3c654c78fc8f3d0c16a9a6134b8eee9e
Italo Guajardo <italog.penaloza@gmail.com>
2021-09-04 13:44:32 -0400

    show usage of memo in Small.js

--- src/components/06-memos/Small.js
+++ src/components/06-memos/Small.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
-export const Small = ({ counter }) => {
+
+// memo memoriza un elemento, y solo si cambió se volverá a renderizar, como un caché
+export const Small = React.memo(({ counter }) => {
 
     // Se creó un nuevo componente para que se renderizara cada vez que cambia el valor de counter
     console.log('Me volví a renderizar');
@@ -10,4 +12,4 @@ export const Small = ({ counter }) => {
             { counter }
         </small>
     )
-}
+})
