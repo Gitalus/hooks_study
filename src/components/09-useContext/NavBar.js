3544e99663acee97cd9e36743df2527d531dbeab
Italo Guajardo <italog.penaloza@gmail.com>
2021-09-07 18:28:29 -0300

    change Links to NavLinks to apply a different class

--- src/components/09-useContext/NavBar.js
+++ src/components/09-useContext/NavBar.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export const NavBar = () => {
     return (
+        // Los NavLinks permiten acceder a la ruta en la que nos encontramos para poder aplicar una clase de css
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">useContext</Link>
+                <NavLink className="navbar-brand" to="/">useContext</NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
-                        <Link className="nav-link" to="/">Home</Link>
-                        <Link className="nav-link" to="/about">About</Link>
-                        <Link className="nav-link" to="/login">Login</Link>
+                        <NavLink activeClassName="active" className="nav-link" to="/">Home</NavLink>
+                        <NavLink activeClassName="active" className="nav-link" to="/about">About</NavLink>
+                        <NavLink activeClassName="active" className="nav-link" to="/login">Login</NavLink>
                     </div>
                 </div>
             </div>
