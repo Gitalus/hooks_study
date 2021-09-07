9a68316e60cf66368f39f3b69351611f52becdcb
Italo Guajardo <italog.penaloza@gmail.com>
2021-09-07 17:30:25 -0300

    create basic usage of react router with different components

--- src/components/09-useContext/AppRouter.js
+++ src/components/09-useContext/AppRouter.js
@@ -4,11 +4,26 @@ import {
     Switch,
     Route,
 } from 'react-router-dom';
+import { AboutScreen } from './AboutScreen';
+import { HomeScreen } from './HomeScreen';
+import { LoginScreen } from './LoginScreen';
 
 export const AppRouter = () => {
     return (
-        <div>
-            
-        </div>
+        <Router>
+            <div>
+                <Switch>
+                    {/* Si se pone el home primero, cualquier ruta que pongamos
+                    nos redigirá al home, ya que devuelve la primera coincidencia,
+                    que sería un "/" */}
+                    {/* Se puede añadir la propiedad exact (exact={true},pero no es necesario).
+                    Esto para que la ruta tiene que ser exacta, y evitar que nos mande al home con
+                    cualquier ruta. */}
+                    <Route exact path="/about" component={ AboutScreen } />
+                    <Route exact path="/login" component={ LoginScreen } />
+                    <Route exact path="/" component={ HomeScreen } />
+                </Switch>
+            </div>
+        </Router>
     )
 }
