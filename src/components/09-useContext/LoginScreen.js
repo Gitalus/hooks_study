8b8ce892df6b0359a0a076dc7ce022463724648a
Italo Guajardo <italog.penaloza@gmail.com>
2021-09-07 20:20:37 -0300

    create button on login que change the state on the attribute value from context

--- src/components/09-useContext/LoginScreen.js
+++ src/components/09-useContext/LoginScreen.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from './UserContext'
 
 export const LoginScreen = () => {
+
+    const { setUser } = useContext(UserContext);
+
     return (
         <div>
             <h1>Login Screen</h1>
             <hr />
+
+            <button
+                className="btn btn-primary"
+                onClick={() => {
+                    setUser({
+                        user: 'Italo',
+                        email: 'italog.penaloza@gmail.com'
+                    })
+                }}
+            >Login</button>
         </div>
     )
 }
--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,18 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AppRouter } from './AppRouter'
 import { UserContext } from './UserContext'
 
 export const MainApp = () => {
 
-    const user = {
-        id: 1234,
-        mame: 'Italo',
-        email: 'italog.penaloza@gmail.com'
-    }
+    // React disparará una renderización en los hijos que usan el useContext cuando el value cambia
+    const [user, setUser] = useState({});
 
     // Lo que uno quiere compartir con el context debe ser llamado value y su valor un objeto
     return (
-        <UserContext.Provider value={user}>
+        <UserContext.Provider value={{
+            user,
+            setUser
+        }}>
             <AppRouter />
         </UserContext.Provider>
     )
